52c16ff7c7b1cc60845e200e030bd9214d39cadf
Dzikra02 <adzikraqa@student.uns.ac.id>
2023-03-13 12:31:22 +0700

    Tugas 1

---
+++ Tugas 1.py
@@ -0,0 +1,9 @@
+print ("^"*100)
+print ("\n"+"Nama : Adz-dzikra Qurota A'yun")
+print ("NIM   : I0322005"+"\n")
+print ("^"*55 + "\n")
+
+print ("""
+    Perkenalkan nama saya Adz-dzikra Qurota A'yun NIM I0322005. Saya berasal dari Klaten, Klaten terkenal dengan sebutan Kota Bersinar. Kota Klaten terkenal dengan tradisi sebar apem yang hingga kini masih dileastarikan oleh warga setempat. Di kota kelahiran saya terdapat pabrik besar yaitu pabrik karung, tetapi sekarang sudah tidak beroperasi lagi. Keadaan iklim Kota Klaten termasuk iklim tropis dan memiliki gunung berapi yang teraktif di Pulau Jawa yaitu Gunung Merapi. Makanan khas dari Kota Klaten adalah soto dan sop ayam.
+    Harapan saya sebagai lulusan Teknik Industri adalah bisa berkontribusi dalam pembangunan industri di Indonesia dengan ilmu yang saya pelajari. Saya juga berharap mendapatkan perkerjaan sebelum lulus yang sesuai dengan minta saya. Selain itu, saya ingin menyalurkan ilmu yang saya pelajari di teknik Industri kepada sesama dengan cara yang inovatif dan menarik.
+""")
